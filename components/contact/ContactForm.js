4628e8ed65af7f77306fb40db1659e1ac2849da6
Can Kanbur <cankanbur1@gmail.com>
2021-12-13 17:28:34 +0300

    added mongoDb

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -1,7 +1,67 @@
-import React from 'react';
-import styled from './ContactForm.module.css';
-const ContactForm = () => {
-  return <></>;
-};
+import classes from './ContactForm.module.css';
+import { useState } from 'react';
+
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const sendMessage = e => {
+    e.preventDefault();
+
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, message }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+  return (
+    <section className={classes.contact}>
+      <h1>Contact to me</h1>
+      <form className={classes.form} onSubmit={sendMessage}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            id="message"
+            rows="5"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </div>
+
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
+  );
+}
 
 export default ContactForm;
---
+++ pages/api/contact.js
@@ -0,0 +1,38 @@
+import { MongoClient } from 'mongodb';
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
+    if (!name || !email.includes('@') || !message) {
+      res.status(400).send('Missing required fields');
+      return;
+    }
+    const newMessage = {
+      name,
+      email,
+      message,
+    };
+    let client;
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://canAdmin:qrbAeVGc8HZ9ra4C@cluster0.yv7qo.mongodb.net/deneme?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(400).json({ message: 'Could not connect to database', error });
+      return;
+    }
+    const db = client.db();
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(400).json({ message: 'Could not insert message', error });
+      return;
+    }
+    client.close();
+    res
+      .status(200)
+      .json({ message: 'successfuly stored message', message: newMessage });
+  }
+  res.status(200).json({ name: 'John Doe' });
+}
