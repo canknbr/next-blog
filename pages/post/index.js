ea09f0d34562940bfc90b7a51c3a6f3168ad6557
Can Kanbur <cankanbur1@gmail.com>
2021-12-13 20:09:53 +0300

    head tag added

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import AllPost from '../../components/post/allPost';
 import { getAllPosts } from '../../lib/utils';
+import Head from 'next/head';
 
 const AllPostPage = ({ posts }) => {
   return (
     <>
+      <Head>
+        <title>All Posts</title>
+      </Head>
       <AllPost posts={posts} />
     </>
   );
