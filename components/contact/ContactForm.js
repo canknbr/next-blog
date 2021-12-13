6109b71c183a78d998253fe79e0848f94be24879
Can Kanbur <cankanbur1@gmail.com>
2021-12-13 19:56:09 +0300

    notification comp added

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -1,37 +1,104 @@
+import { useState, useEffect } from 'react';
+
 import classes from './ContactForm.module.css';
-import { useState } from 'react';
+import Notification from '../ui/Notification.js';
+
+async function sendContactData(contactDetails) {
+  const response = await fetch('/api/contact', {
+    method: 'POST',
+    body: JSON.stringify(contactDetails),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong!');
+  }
+}
 
 function ContactForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const sendMessage = e => {
-    e.preventDefault();
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredMessage, setEnteredMessage] = useState('');
+  const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
+  const [requestError, setRequestError] = useState();
 
-    fetch('/api/contact', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, message }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    setName('');
-    setEmail('');
-    setMessage('');
-  };
+  useEffect(() => {
+    if (requestStatus === 'success' || requestStatus === 'error') {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
+  async function sendMessageHandler(event) {
+    event.preventDefault();
+
+    // optional: add client-side validation
+
+    setRequestStatus('pending');
+
+    try {
+      await sendContactData({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      });
+      setRequestStatus('success');
+      setEnteredMessage('');
+      setEnteredEmail('');
+      setEnteredName('');
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus('error');
+    }
+  }
+
+  let notification;
+
+  if (requestStatus === 'pending') {
+    notification = {
+      status: 'pending',
+      title: 'Sending message...',
+      message: 'Your message is on its way!',
+    };
+  }
+
+  if (requestStatus === 'success') {
+    notification = {
+      status: 'success',
+      title: 'Success!',
+      message: 'Message sent successfully!',
+    };
+  }
+
+  if (requestStatus === 'error') {
+    notification = {
+      status: 'error',
+      title: 'Error!',
+      message: requestError,
+    };
+  }
+
   return (
     <section className={classes.contact}>
-      <h1>Contact to me</h1>
-      <form className={classes.form} onSubmit={sendMessage}>
+      <h1>How can I help you?</h1>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
             <input
               type="email"
               id="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
               required
+              value={enteredEmail}
+              onChange={event => setEnteredEmail(event.target.value)}
             />
           </div>
           <div className={classes.control}>
@@ -39,9 +106,9 @@ function ContactForm() {
             <input
               type="text"
               id="name"
-              value={name}
-              onChange={e => setName(e.target.value)}
               required
+              value={enteredName}
+              onChange={event => setEnteredName(event.target.value)}
             />
           </div>
         </div>
@@ -50,9 +117,9 @@ function ContactForm() {
           <textarea
             id="message"
             rows="5"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
             required
+            value={enteredMessage}
+            onChange={event => setEnteredMessage(event.target.value)}
           ></textarea>
         </div>
 
@@ -60,6 +127,13 @@ function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 }
--- components/layout/mainNavigation.js
+++ components/layout/mainNavigation.js
@@ -4,6 +4,9 @@ import styled from './mainNav.module.css';
 const mainNavigation = () => {
   return (
     <header className={styled.header}>
+      <div>
+        <Link href="/">Home</Link>
+      </div>
       <ul>
         <li>
           <Link href="/post">Post</Link>
